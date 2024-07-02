03a9e537008162b347a4b29f91e0ddab2b1fdeee
JhingaMan <kunalgupta1208@gmail.com>
2024-07-03 01:20:00 +0530

    ADD the theme and Live Rendering

--- React_Admin/src/scenes/dashboard/chart.jsx
+++ React_Admin/src/scenes/dashboard/chart.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import  {AgCharts}  from "ag-charts-react";
+import { AgCharts } from "ag-charts-react";
 import createSocketConnection from "./data";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
 
 const LiveChart = () => {
     const [chartData, setChartData] = useState([]);
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
     useEffect(() => {
         const socket = createSocketConnection(setChartData);
@@ -17,6 +21,8 @@ const LiveChart = () => {
     const [options, setOptions] = useState({
         title: {
             text: "IOT DATA",
+            fontSize: 24,
+            color: colors.grey[100],
         },
         data: chartData,
         series: [
@@ -25,13 +31,48 @@ const LiveChart = () => {
                 xKey: "ID",
                 yKey: "TEMPERATURE",
                 yName: "Temperature",
+                stroke: colors.redAccent[500],
+                marker: {
+                    fill: colors.redAccent[300],
+                },
             },
             {
                 type: "line",
                 xKey: "ID",
                 yKey: "RAM_USAGE",
                 yName: "RAM Usage",
+                stroke: colors.greenAccent[500],
+                marker: {
+                    fill: colors.greenAccent[300],
+                },
             },
+        ],
+        background: {
+            fill: colors.primary[400],
+        },
+        axes: [
+            {
+                type: 'category',
+                position: 'bottom',
+                label: {
+                    color: colors.grey[100],
+                },
+                title: {
+                    color: colors.grey[100],
+                    text : "ID"
+                }
+            },
+            {
+                type: 'number',
+                position: 'left',
+                label: {
+                    color: colors.grey[100],
+                },
+                title: {
+                    color: colors.grey[100],
+                    text : "data",
+                }
+            }
         ],
     });
 
@@ -43,6 +84,6 @@ const LiveChart = () => {
     }, [chartData]);
 
     return <AgCharts options={options} />;
-}
+};
 
 export default LiveChart;
--- React_Admin/src/scenes/dashboard/index.jsx
+++ React_Admin/src/scenes/dashboard/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import LiveChart from './chart'
-
+import './index.css'
 const index = () => {
   return (
-    <div>
+    <div className='iotData'>
       <LiveChart />
     </div>
   )

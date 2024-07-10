553357d3084eef89ac739a2ac1fb087f75c27818
JhingaMan <kunalgupta1208@gmail.com>
2024-07-10 13:07:46 +0530

    updated

--- React_Admin/src/scenes/dashboard/index.jsx
+++ React_Admin/src/scenes/dashboard/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import LiveChart from './chart'
-import './index.css'
-const index = () => {
+import React from 'react';
+import './index.css';
+
+const Index = () => {
   return (
-    <div className='iotData'>
-      <LiveChart />
+    <div>
+      <iframe 
+        src="http://localhost:3000/d-solo/ddqj959mhsqv4a/nodes-index?orgId=1&refresh=5s&from=1720593518081&to=1720593818081&panelId=35" 
+        width="450" 
+        height="200" 
+        frameBorder="0"
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default index
+export default Index;
